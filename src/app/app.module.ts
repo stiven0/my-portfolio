3622b3a7cd620be723e4a32669d5866f3a256049
stiven <stiven2750@gmail.com>
2023-02-23 18:34:48 -0500

    feat: the operation of 'pwa' is added and the 'service-worker' is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AppRoutingModule } from '@app/app.routing.module';
 
 // pages
 import { HomeComponent } from '@pages/home/home.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,13 @@ import { HomeComponent } from '@pages/home/home.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
